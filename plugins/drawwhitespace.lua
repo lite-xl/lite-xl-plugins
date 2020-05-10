3c3a68b84bcbd9f087885454453b0c901c80a86c
rxi <rxi@users.noreply.github.com>
2020-05-10 08:16:34 +0100

    Updated drawwhitespace plugin not to use old `get_cached_line` func
    Fixes #23

--- plugins/drawwhitespace.lua
+++ plugins/drawwhitespace.lua
@@ -11,14 +11,14 @@ local draw_line_text = DocView.draw_line_text
 function DocView:draw_line_text(idx, x, y)
   draw_line_text(self, idx, x, y)
 
-  local cl = self:get_cached_line(idx)
+  local text = self.doc.lines[idx]
   local tx, ty = x, y + self:get_line_text_y_offset()
   local font = self:get_font()
   local color = style.whitespace or style.syntax.comment
   local map = config.whitespace_map
 
-  for i = 1, #cl.text do
-    local chr = cl.text:sub(i, i)
+  for i = 1, #text do
+    local chr = text:sub(i, i)
     local rep = map[chr]
     if rep then
       renderer.draw_text(font, rep, tx, ty, color)
