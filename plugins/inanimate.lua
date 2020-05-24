3b3253f8897c406ffb2d93d2b931df33c934d797
rxi <rxi@users.noreply.github.com>
2020-05-24 08:01:13 +0100

    Added setting of `core.redraw` in inanimate plugin
    Fixes #43

--- plugins/inanimate.lua
+++ plugins/inanimate.lua
@@ -1,8 +1,12 @@
+local core = require "core"
 local View = require "core.view"
 
 function View:move_towards(t, k, dest)
   if type(t) ~= "table" then
     return self:move_towards(self, t, k, dest)
+  end
+  if t[k] ~= dest then
+    core.redraw = true
   end
   t[k] = dest
 end
