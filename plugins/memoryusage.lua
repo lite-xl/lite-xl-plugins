34c911d9b254c1e9b3a68b050808584930e98797
Francesco Abbate <francesco.bbt@gmail.com>
2021-04-10 11:41:27 +0200

    Add memory usage plugin
    Original implementation from AqilCont:
    
    https://github.com/franko/lite-xl/pull/108

---
+++ plugins/memoryusage.lua
@@ -0,0 +1,18 @@
+-- original implementation by AqilCont
+local style = require "core.style"
+local StatusView = require "core.statusview"
+
+local get_items = StatusView.get_items
+
+function StatusView:get_items()
+  local left, right = get_items(self)
+  local t = {
+    style.text, (math.floor(collectgarbage("count") / 10.24) / 100) .. " MB",
+    style.dim, self.separator2,
+  }
+  for i, item in ipairs(t) do
+    table.insert(right, i, item)
+  end
+  return left, right
+end
+
