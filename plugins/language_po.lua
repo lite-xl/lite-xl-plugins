57adcb345c4a14de9cf69a3f4f971455732a95ab
jgmdev <jgmdev@gmail.com>
2021-06-14 19:04:47 -0400

    Added language_po

---
+++ plugins/language_po.lua
@@ -0,0 +1,20 @@
+-- mod-version:1 -- lite-xl 1.16
+local syntax = require "core.syntax"
+
+syntax.add {
+  files = { "%.po$", "%.pot$" },
+  comment = "#",
+  patterns = {
+    { pattern = { "#", "\n"},        type = "comment"  },
+    { pattern = { '"', '"', '\\' },  type = "string"   },
+    { pattern = "[%[%]]",            type = "operator" },
+    { pattern = "%d+",               type = "number"   },
+    { pattern = "[%a_][%w_]*",       type = "symbol"   },
+  },
+  symbols = {
+    ["msgctxt"]      = "keyword",
+    ["msgid"]        = "keyword",
+    ["msgid_plural"] = "keyword",
+    ["msgstr"]       = "keyword",
+  },
+}
