3d3c48f3215c1d663abafefd78a0ca595565df0e
ajalexei <23712361+ajalexei@users.noreply.github.com>
2021-06-15 23:07:59 +0900

    Uploaded files
    added language_bib.lua, pdfview.lua, texcompile.lua

---
+++ plugins/pdfview.lua
@@ -0,0 +1,45 @@
+-- mod-version:1 -- lite-xl 1.16
+local core = require "core"
+local command = require "core.command"
+local keymap = require "core.keymap"
+
+command.add("core.docview", {
+  ["pdfview:show-preview"] = function()
+    local av = core.active_view
+
+-- User's home directory
+    local homedir = ""
+
+    if PLATFORM == "Windows" then
+        homedir = os.getenv("USERPROFILE")
+    else
+        homedir = os.getenv("HOME")
+    end
+
+-- The current (La)TeX file
+    local texfile = av:get_filename()
+    texfile = string.gsub(texfile, '~', homedir)
+-- Construct the PDF file name out of the (La)Tex filename
+    local pdffile = string.gsub(texfile, ".tex", ".pdf")
+-- PDF viewer - is there any provided by the environment
+    local pdfcmd = os.getenv("LITE_PDF_VIEWER")
+
+    core.log("Opening pdf preview for \"%s\"", texfile)
+
+    if pdfcmd ~= nil then
+        system.exec(pdfcmd .. " " .. pdffile)
+    elseif PLATFORM == "Windows" then
+        system.exec("start " .. pdffile)
+    else
+        system.exec(string.format("xdg-open %q", pdffile))
+    end
+
+--    core.add_thread(function()
+--      coroutine.yield(5)
+--      os.remove(htmlfile)
+--    end)
+  end
+})
+
+
+keymap.add { ["ctrl+shift+v"] = "pdfview:show-preview" }
