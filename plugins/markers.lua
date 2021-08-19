2d2fd65a8c18525d92d86f4690dad5cbcdb74f05
Adam Harrison <adamdharrison@gmail.com>
2021-08-19 09:25:25 -0400

    Also fixed markers to add width.

--- plugins/markers.lua
+++ plugins/markers.lua
@@ -52,12 +52,12 @@ end
 
 local draw_line_gutter = DocView.draw_line_gutter
 
-function DocView:draw_line_gutter(idx, x, y)
+function DocView:draw_line_gutter(idx, x, y, width)
   if cache[self.doc] and cache[self.doc][idx] then
     local h = self:get_line_height()
     renderer.draw_rect(x, y, style.padding.x * 0.4, h, style.selection)
   end
-  draw_line_gutter(self, idx, x, y)
+  draw_line_gutter(self, idx, x, y, width)
 end
 
 
