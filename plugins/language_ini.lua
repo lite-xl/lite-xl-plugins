42c4f8ea784e49c4a1e1cb37cf8b21e68c4802e4
Duncan Lock <duncan.lock@gmail.com>
2021-10-08 05:19:36 -0700

    Add syntax highlighting for .ini files

---
+++ plugins/language_ini.lua
@@ -0,0 +1,26 @@
+-- mod-version:2
+
+local syntax = require "core.syntax"
+
+syntax.add {
+  files = { "%.ini$", "%.inf$", "%.cfg$", "%.editorconfig$" },
+  comment = ';',
+  patterns = {
+    { pattern = ";.-\n", type = "comment" },
+    { pattern = "#.-\n", type = "comment" },
+    { pattern = { "%[", "%]" }, type = "keyword" },
+
+    { pattern = { '"""', '"""', '\\' }, type = "string" },
+    { pattern = { '"', '"', '\\' }, type = "string" },
+    { pattern = { "'''", "'''" }, type = "string" },
+    { pattern = { "'", "'" }, type = "string" },
+    { pattern = "[A-Za-z0-9_%.%-]+%s*%f[=]", type = "function" },
+    { pattern = "[%-+]?[0-9_]+%.[0-9_]+", type = "number" },
+    { pattern = "[%-+]?[0-9_]+", type = "number" },
+    { pattern = "[a-z]+", type = "symbol" },
+  },
+  symbols = {
+    ["true"] = "literal",
+    ["false"] = "literal",
+  },
+}
