dc64ff09521cf98a9adc44be8c5a019d62e7aa17
Guldoman <giulio.lettieri@gmail.com>
2021-11-09 21:46:13 +0100

    `extend_selection_line`: Draw line body before drawing extension
    This avoids problems such as drawing the line highlight over the 
    extension.
    This will probably conflict with other plugins that write after the end 
    of the line.

--- plugins/extend_selection_line.lua
+++ plugins/extend_selection_line.lua
@@ -4,6 +4,7 @@ local style = require "core.style"
 
 local draw_line_body = DocView.draw_line_body
 function DocView:draw_line_body(idx, x, y, ...)
+  draw_line_body(self, idx, x, y, ...)
   local lh = self:get_line_height()
   for _, line1, _, line2, _ in self.doc:get_selections(true) do
     if idx >= line1 and idx < line2 and line1 ~= line2 then
@@ -15,5 +16,4 @@ function DocView:draw_line_body(idx, x, y, ...)
       end
     end
   end
-  draw_line_body(self, idx, x, y, ...)
 end
