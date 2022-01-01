e86368227156c70b44c7d3927dbcb96b58d96da2
takase1121 <20792268+takase1121@users.noreply.github.com>
2022-01-01 13:36:18 +0800

    export README into json

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -4,14 +4,15 @@ import centra from 'centra'
 import pLimit from 'p-limit'
 import { remark } from 'remark'
 import remarkGfm from 'remark-gfm'
+import strip from 'strip-markdown'
 import { select, selectAll } from 'unist-util-select'
 
-const baseurl = process.argv[2]
-const inputFile = process.argv[3]
-const outputFile = process.argv[4]
+const configFile = process.argv[2]
+
+const clone = obj => JSON.parse(JSON.stringify(obj))
 
 const selector = 'tableCell:first-child inlineCode'
-const sortEntries = async tree => {
+const sortEntries = () => async tree => {
 	const rows = select('table', tree)
 	const header = rows.children.shift()
 
@@ -23,12 +24,13 @@ const sortEntries = async tree => {
 	return tree
 }
 
-const makeUrl = link => link.url.startsWith('http') ? link.url : baseurl + link.url
+const makeUrl = baseUrl => link => link.url.startsWith('http') ? link.url : baseUrl + link.url
 
-const get404s = async tree => {
+const get404s = baseUrl => async tree => {
+	const url = makeUrl(baseUrl)
 	const rows = select('table', tree)
 	const links = selectAll('tableCell:first-child link', rows)
-		.map(makeUrl)
+		.map(url)
 
 
 	const limit = pLimit(5)
@@ -53,22 +55,60 @@ const get404s = async tree => {
 	rows.children = rows.children
 		.filter(row => {
 			const link = select('tableCell:first-child link', row)
-			return link ? good.has(makeUrl(link)) : true
+			return link ? good.has(url(link)) : true
 		})
 
 
 	return tree
 }
 
+const stripMd = strip({ keep: ['tableCell'] })
+const exportData = options => async tree => {
+	const { baseUrl, output } = options
+	const url = makeUrl(baseUrl)
+	const rows = selectAll('table tableRow', tree)
+	rows.shift()
+
+	const md = remark().use(remarkGfm)
+	for (const row of rows) {
+		const nameCol = select('tableCell:first-child', row)
+		const descCol = select('tableCell:nth-child(2)', row)
+
+		// we actually need to clone because strip alters AST
+		const name = stripMd(clone(nameCol)).children[0].value
+		const desc = stripMd(clone(descCol)).children[0].value
+
+		const realName = name.replace(/\*$/, '')
+		output[realName] = {
+			name: realName,
+			external: name.endsWith('*'),
+			description: desc,
+			rawDescription: md.stringify(descCol),
+			url: url(select('link', nameCol))
+		}
+	}
+}
+
 const main = async () => {
+	const config = JSON.parse(await readFile(configFile, { encoding: 'utf-8' }))
+	const {
+		baseUrl,
+		inputFile,
+		outputFile,
+		pluginFile
+	} = config
+
 	const file = await readFile(inputFile, { encoding: 'utf-8' })
+	const plugins = {}
 	const output = await remark()
 		.use(remarkGfm)
-		.use(() => get404s)
-		.use(() => sortEntries)
+		.use(get404s, baseUrl)
+		.use(sortEntries)
+		.use(exportData, { baseUrl, output: plugins })
 		.process(file)
 
 	await writeFile(outputFile, String(output))
+	await writeFile(pluginFile, JSON.stringify(plugins))
 }
 
 main()
