291dac89ebc29457073d6ff54792ab049c1dafa1
jgmdev <jgmdev@gmail.com>
2022-02-11 02:35:28 -0400

    findfileimproved: added thread module support.

--- plugins/findfileimproved.lua
+++ plugins/findfileimproved.lua
@@ -6,12 +6,20 @@ local common = require "core.common"
 local command = require "core.command"
 local StatusView = require "core.statusview"
 
+---@type thread
+local threads = nil
+if pcall(require, "thread") then
+  threads = require "thread"
+end
+
 local project_files = {}
 local refresh_files = false
 local matching_files = 0
 local project_total_files = 0
 local loading_text = ""
 local project_directory = ""
+local coroutine_running = false
+
 
 local function basedir_files()
   local files = system.list_dir(project_directory)
@@ -38,13 +46,181 @@ local function basedir_files()
   return files_return
 end
 
-local function index_files_thread()
+
+local function update_suggestions()
+  if
+    core.active_view == core.command_view
+    and
+    core.command_view.label == "Open File From Project: "
+  then
+    core.command_view:update_suggestions()
+  end
+end
+
+
+local function update_loading_text(init)
+  if init then
+    loading_text = "[-]"
+    return
+  elseif type(init) == "boolean" then
+    loading_text = "Matches:"
+    return
+  end
+
+  if loading_text == "[-]" then
+    loading_text = "[\\]"
+  elseif loading_text == "[\\]" then
+    loading_text = "[|]"
+  elseif loading_text == "[|]" then
+    loading_text = "[/]"
+  elseif loading_text == "[/]" then
+    loading_text = "[-]"
+  end
+end
+
+
+local function index_files_thread(pathsep, ignore_files)
+  local function match_pattern(text, pattern, ...)
+    if type(pattern) == "string" then
+      return text:find(pattern, ...)
+    end
+    for _, p in ipairs(pattern) do
+      local s, e = match_pattern(text, p, ...)
+      if s then return s, e end
+    end
+    return false
+  end
+
+  ---@type thread.Channel
+  local input = thread.getChannel("findfileimproved_write")
+  ---@type thread.Channel
+  local output = thread.getChannel("findfileimproved_read")
+
+  local root = input:wait()
+  input:pop()
+
+  output:push("indexing")
+
+  local count = 0
+  local directories = {""}
+  local files_found = {}
+
+  while #directories > 0 do
+    for didx, directory in ipairs(directories) do
+      local dir_path = ""
+
+      if directory ~= "" then
+        dir_path = root .. pathsep .. directory
+        directory = directory .. pathsep
+      else
+        dir_path = root
+      end
+
+      local files = system.list_dir(dir_path)
+
+      if files then
+        for _, file in ipairs(files) do
+          local info = system.get_file_info(
+            dir_path .. pathsep .. file
+          )
+
+          if
+            not match_pattern(
+              file, ignore_files
+            )
+          then
+            if info.type == "dir" then
+              table.insert(directories, directory .. file)
+            else
+              table.insert(files_found, directory .. file)
+            end
+          end
+        end
+      end
+      table.remove(directories, didx)
+      break
+    end
+
+    count = count + 1
+    if count % 500 == 0 then
+      output:push(files_found)
+      files_found = {}
+    end
+  end
+
+  if #files_found > 0 then
+    output:push(files_found)
+  end
+
+  output:push("finished")
+end
+
+
+local function index_files_coroutine()
   while true do
-    if refresh_files then
+    -- Indexing with thread module/plugin
+    if threads and refresh_files then
+      ---@type thread.Channel
+      local input = threads.getChannel("findfileimproved_read")
+      ---@type thread.Channel
+      local output = threads.getChannel("findfileimproved_write")
+
+      -- Tell the thread to start indexing the pushed directory
+      output:push(project_directory)
+      local count = 0
+
+      local indexing_thread = threads.create(
+        "findfileimproved", index_files_thread, PATHSEP, config.ignore_files
+      )
+
+      while refresh_files do
+        local value = input:first()
+        count = count + 1
+
+        if value then
+          local value_type = type(value)
+          if value_type == "string" then
+            if value == "indexing" then
+              project_files = {}
+              update_loading_text(true)
+            elseif value == "finished" then
+              refresh_files = false
+              update_loading_text(false)
+              update_suggestions()
+            end
+          elseif value_type == "table" then
+            for _, file in ipairs(value) do
+              table.insert(project_files, file)
+            end
+          end
+          input:pop()
+        end
+
+        if refresh_files then
+          local total_project_files = #project_files
+          if total_project_files ~= project_total_files then
+            project_total_files = total_project_files
+            if count % 10000 == 0 then
+              update_suggestions()
+            end
+          end
+        end
+
+        if count % 100 == 0 then
+          update_loading_text()
+          coroutine.yield()
+        end
+      end
+
+      coroutine_running = false
+
+      return
+    -- Indexing without thread module
+    elseif refresh_files then
       refresh_files = false
 
       project_files = {}
-      loading_text = "[-]"
+      update_loading_text(true)
 
       local count = 0
       local suggestions_updated = false
@@ -93,45 +269,48 @@ local function index_files_thread()
 
         count = count + 1
         if count % 100 == 0 then
-          if
-            core.active_view == core.command_view
-            and
-            core.command_view.label == "Open File From Project: "
-          then
-            core.command_view:update_suggestions()
+          update_loading_text()
+
+          if count % 10000 == 0 then
+            update_suggestions()
             suggestions_updated = true
           end
 
-          if loading_text == "[-]" then
-            loading_text = "[\\]"
-          elseif loading_text == "[\\]" then
-            loading_text = "[|]"
-          elseif loading_text == "[|]" then
-            loading_text = "[/]"
-          elseif loading_text == "[/]" then
-            loading_text = "[-]"
-          end
-
-          coroutine.yield(0)
+          coroutine.yield()
         end
       end
 
-      loading_text = "Matches:"
+      update_loading_text(false)
 
-      if
-        not suggestions_updated
-        and
-        core.active_view == core.command_view
-        and
-        core.command_view.label == "Open File From Project: "
-      then
-        core.command_view:update_suggestions()
+      if not suggestions_updated then
+        update_suggestions()
       end
     else
       coroutine.yield(2)
     end
   end
 end
+
+
+local function fuzzy_match_extended(files, visited, text)
+  local results = common.fuzzy_match_with_recents(files, visited, text)
+
+  local last_char = text:sub(text:len())
+
+  if last_char == "/" or last_char == "\\" then
+    local results_copy = {}
+    for _, result in ipairs(results) do
+      local idx = result:find(text)
+      if idx then
+        table.insert(results_copy, result)
+      end
+    end
+    results = results_copy
+  end
+
+  return results
+end
+
 
 local function open_project_file()
   local project_dir_changed = false
@@ -145,8 +324,10 @@ local function open_project_file()
     return
   end
 
-  if #project_files <= 0 or project_dir_changed then
-    refresh_files = true
+  refresh_files = true
+  if threads and not coroutine_running then
+    coroutine_running = true
+    core.add_thread(index_files_coroutine)
   end
 
   core.command_view:enter("Open File From Project",
@@ -156,11 +337,11 @@ local function open_project_file()
     function(text)
       local results = {}
       if text == "" or #project_files == 0 then
-        results = common.fuzzy_match_with_recents(
+        results = fuzzy_match_extended(
           base_files, core.visited_files, text
         )
       else
-        results = common.fuzzy_match_with_recents(
+        results = fuzzy_match_extended(
           project_files, core.visited_files, text
         )
       end
@@ -175,7 +356,6 @@ end
 --
 
 local status_view_get_items = StatusView.get_items
-local core_on_dir_change = core.on_dir_change
 
 -- display amount of files and matching results
 function StatusView:get_items()
@@ -200,16 +380,9 @@ function StatusView:get_items()
   return left, right
 end
 
--- register the indexing thread
-core.add_thread(index_files_thread)
-
--- re-index project files on dir change events
-core.on_dir_change = function(watch_id, action, filepath)
-  core_on_dir_change(watch_id, action, filepath)
-  if action ~= "modify" then
-    project_directory = core.project_dir
-    refresh_files = true
-  end
+-- register the indexing coroutine
+if not threads then
+  core.add_thread(index_files_coroutine)
 end
 
 -- overwrite core:find-file function
