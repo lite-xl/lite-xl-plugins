b68091bf1fb3f0935390f0d4bb38e55194f18ade
jgmdev <jgmdev@gmail.com>
2022-03-11 05:08:59 -0400

    unboundedscroll: added command to toggle

--- plugins/unboundedscroll.lua
+++ plugins/unboundedscroll.lua
@@ -1,6 +1,18 @@
 -- mod-version:3 --lite-xl 2.1
+local command = require "core.command"
 local DocView = require "core.docview"
 
-function DocView.clamp_scroll_position()
-  -- do nothing
-end
+local doc_view_clamp_scroll_position = DocView.clamp_scroll_position
+local function clamp_scroll_noop() end
+
+DocView.clamp_scroll_position = clamp_scroll_noop
+
+command.add(nil, {
+  ["unbounded-scroll:toggle"] = function()
+    if DocView.clamp_scroll_position == clamp_scroll_noop then
+      DocView.clamp_scroll_position = doc_view_clamp_scroll_position
+    else
+      DocView.clamp_scroll_position = clamp_scroll_noop
+    end
+  end,
+})
