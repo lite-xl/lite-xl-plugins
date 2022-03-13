df63ba11449b08579ea5491793cccbd740b7f8b9
jgmdev <jgmdev@gmail.com>
2022-03-13 14:54:55 -0400

    hidelinenumbers: removed, the linenumbers plugin provides a better alternative

--- plugins/hidelinenumbers.lua
+++
@@ -1,6 +0,0 @@
--- mod-version:3 --lite-xl 2.1
-local style = require "core.style"
-local DocView = require "core.docview"
-
-DocView.draw_line_gutter = function() end
-DocView.get_gutter_width = function() return style.padding.x end
