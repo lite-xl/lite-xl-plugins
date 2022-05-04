fbc2bc9267e2d6f1202aac747f90e339518b1564
Adam Harrison <adamdharrison@gmail.com>
2022-05-03 20:05:55 -0400

    Added in word count on selection.

--- plugins/wordcount.lua
+++ plugins/wordcount.lua
@@ -16,7 +16,7 @@ local function compute_line_words(line)
   while true do 
     local ns, e = line:find("%s+", s)
     if ns == 1 and e == #line then break end
-    if not e then break end
+    if not e then total_words = math.max(total_words, 1) break end
     total_words = total_words + 1
     s = e + 1
   end
@@ -69,12 +69,22 @@ function Doc:new(...)
   words[self] = compute_words(self)
 end
 
+local cached_word_length, cached_word_count
 
 core.status_view:add_item(
   function() return core.active_view:is(DocView) and not core.active_view:is(CommandView) and words[core.active_view.doc] end,
   "status:word-count",
   StatusView.Item.RIGHT,
   function()
-    return { style.text, words[core.active_view.doc] .. " words" }
+    local selection_text = core.active_view.doc:get_selection_text()
+    if #selection_text ~= cached_word_length then
+      cached_word_count = compute_line_words(selection_text)
+      cached_word_length = #selection_text
+    end
+    if #selection_text > 0 then
+      return { style.text, cached_word_count .. " words" }
+    else
+      return { style.text, words[core.active_view.doc] .. " words" }
+    end
   end
 )
