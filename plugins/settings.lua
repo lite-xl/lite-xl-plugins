adf5a6a871ed0d2852a14a3b6b9e163abf1651ce
jgmdev <jgmdev@gmail.com>
2022-05-19 23:38:33 -0400

    settings: custom drawing of colors preview

--- plugins/settings.lua
+++ plugins/settings.lua
@@ -609,6 +609,16 @@ local function get_installed_colors()
         table.sort(colors, function(a, b)
           return a[1] + a[2] + a[3] < b[1] + b[2] + b[3]
         end)
+        -- remove duplicate colors
+        local b = {}
+        for i = #colors, 1, -1 do
+          local a = colors[i]
+          if a[1] == b[1] and a[2] == b[2] and a[3] == b[3] then
+            table.remove(colors, i)
+          else
+            b = colors[i]
+          end
+        end
         -- insert color to ordered table if not duplicate
         filename = filename:gsub("%.lua$", "")
         if not files[filename] then
@@ -974,6 +984,29 @@ function Settings:load_core_settings()
   end
 end
 
+---@param self widget.listbox
+---@oaram row integer
+---@param x integer
+---@param y integer
+---@param font renderer.font
+---@param color renderer.color
+---@param only_calc boolean
+local function on_color_draw(self, row, x, y, font, color, only_calc)
+  local w = self:get_width() - (x - self.position.x) - style.padding.x
+  local h = font:get_height()
+
+  if not only_calc then
+    local row_data = self:get_row_data(row)
+    local width = w/#row_data.colors
+
+    for i = 1, #row_data.colors do
+      renderer.draw_rect(x + ((i - 1) * width), y, width, h, row_data.colors[i])
+    end
+  end
+
+  return w, h
+end
+
 ---Generate the list of all available colors with preview
 function Settings:load_color_settings()
   self.colors.scrollable = false
@@ -994,19 +1027,14 @@ function Settings:load_color_settings()
     if settings.config.theme and settings.config.theme == name then
       listbox:set_selected(idx)
     end
-    local palette = {}
-    for _, color in ipairs(details.colors) do
-      table.insert(palette, color)
-      table.insert(palette, "█")
-    end
     listbox:add_row({
-      style.text, name, ListBox.COLEND, style.code_font, table.unpack(palette)
-    }, name)
+      style.text, name, ListBox.COLEND, on_color_draw
+    }, {name = name, colors = details.colors})
   end
 
   function listbox:on_row_click(idx, data)
-    core.reload_module("colors." .. data)
-    settings.config.theme = data
+    core.reload_module("colors." .. data.name)
+    settings.config.theme = data.name
   end
 end
 
