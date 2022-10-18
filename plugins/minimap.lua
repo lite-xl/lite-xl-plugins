c15e6014c6faa94b4bdde8e1020467d056de2c7d
Adam <adamdharrison@gmail.com>
2022-10-18 17:30:01 -0400

    ??

--- plugins/minimap.lua
+++ plugins/minimap.lua
@@ -391,19 +391,26 @@ end
 
 function MiniMap:on_mouse_pressed(button, x, y, clicks)
   local percent = MiniMap.super.on_mouse_pressed(self, button, x, y, clicks)
-  if not self:is_minimap_enabled() or type(percent) ~= "number" then return percent end
+  if not self:is_minimap_enabled() or not percent then return percent end
   local _, visible_lines_count, minimap_lines_start, minimap_lines_count, is_file_too_large = self:get_minimap_dimensions()
-  return ((percent * minimap_lines_count) + minimap_lines_start) / #self.dv.doc.lines
+  local _, _, w, h = self:get_track_rect()
+  local lh = self.dv:get_line_height()
+  percent = math.max(0.0, math.min((y - self.dv.position.y) / h, 1.0))
+  return (((percent * minimap_lines_count) + minimap_lines_start) * lh / self.dv:get_scrollable_size()) - (visible_lines_count / #self.dv.doc.lines / 2)
 end
 
 
 function MiniMap:on_mouse_moved(x, y, dx, dy)
   local percent = MiniMap.super.on_mouse_moved(self, x, y, dx, dy)
-  if not self:is_minimap_enabled() or type(percent) ~= "number"  then return percent end
+  if not self:is_minimap_enabled() or type(percent) ~= "number" then return percent end
   local _, _, _, _, is_file_too_large = self:get_minimap_dimensions()
-  local x, y, w, h = self:get_track_rect()
+  local _, _, w, h = self:get_track_rect()
   local _, _, _, th = self:get_thumb_rect()
-  return percent * (self.dv.size.y / (h + (not is_file_too_large and th or 0)))
+  if is_file_too_large then
+    return percent * (self.dv.size.y / h)
+  else
+    return percent * (self.dv.size.y / (h + th))
+  end
 end
 
 
