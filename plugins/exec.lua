eb26a0ff0c7b3cc32b93c58e0a28e7c4c4257ca7
Alejandro Torras <1059709+atorr0@users.noreply.github.com>
2023-01-01 23:52:43 +0100

    REFACTOR io.popen use to process.start

--- plugins/exec.lua
+++ plugins/exec.lua
@@ -2,6 +2,7 @@
 local core = require "core"
 local command = require "core.command"
 local DocView = require "core.docview"
+local process = require "process"
 
 
 local function exec(cmd, keep_newline)
@@ -12,18 +13,16 @@ local function exec(cmd, keep_newline)
 end
 
 
-local function execw(cmd, v, keep_newline)
-  local fp = io.popen(cmd, "w")
-  fp:write(tostring(v))
-  fp:close()
-end
-
-
-local function exec_doc(dv, cmd, keep_newline)
-  local tmp = exec("mktemp -t lite-xl.tmp.XXXXXXXXXX", false)
-  execw(cmd .. " 2>&1 >" .. tmp, dv, keep_newline)
-
-  return exec("cat " .. tmp, keep_newline)
+local function exec_rw(dv, cmd, keep_newline)
+  local proc = process.start { "sh", "-c", cmd }
+  proc:write(tostring(dv))
+  local res = ""
+  while true do
+    local rdbuf = proc:read_stdout()
+    if not rdbuf then break end
+    res = res .. rdbuf
+  end
+  return keep_newline and res or res:gsub("%\n$", "")
 end
 
 
@@ -68,7 +67,7 @@ command.add("core.docview", {
     core.command_view:enter("Replace With Result Of Command From Piped Document Content", {
       submit = function(cmd)
         dv.doc:replace(function(str)
-          return exec_doc(dv, 
+          return exec_rw(dv, 
             "printf %b " .. printfb_quote(str:gsub("%\n$", "") .. "\n") .. " | eval '' " .. shell_quote(cmd),
             str:find("%\n$")
           )
