b40a4d55f1ddba7a74aa08df36a9a111588e5baa
ThaCuber <70547062+ThaCuber@users.noreply.github.com>
2023-05-04 22:56:57 -0400

    changed to common.lerp and fixed underline trail

--- plugins/motiontrail.lua
+++ plugins/motiontrail.lua
@@ -33,17 +33,10 @@ config.plugins.motiontrail = common.merge({
 local cc_installed = pcall(require, 'plugins.custom_caret')
 local cc_conf = config.plugins.custom_caret
 
-local function lerp(a, b, t)
-  if not (a and b and t) then return 0 end
-  return a + (b - a) * t
-end
-
-local caret_index = 1
-
-local function get_caret_rect(dv)
-  local line, col = dv.doc:get_selection_idx(caret_index)
+local function get_caret_size(dv, i)
+  local line, col = dv.doc:get_selection_idx(i)
   local chw = dv:get_font():get_width(dv.doc:get_char(line, col))
-   local w = style.caret_width
+  local w = style.caret_width
   local h = dv:get_line_height()
 
   if cc_installed then
@@ -60,39 +53,48 @@ local function get_caret_rect(dv)
 end
 
 local last_x, last_y, last_view = {}, {}, {}
+local caret_idx = 1
 
 local dv_update = DocView.update
 function DocView:update()
-  caret_index = 1
+  caret_idx = 1
   dv_update(self)
 end
 
 local dv_draw_caret = DocView.draw_caret
-function DocView:draw_caret(x,y)
+function DocView:draw_caret(x, y)
   if not config.plugins.motiontrail.enabled or self ~= core.active_view then
     dv_draw_caret(self, x, y)
     return
   end
 
-  local lsw, lsx, lsy = last_view[caret_index], last_x[caret_index], last_y[caret_index]
-  local w, h = get_caret_rect(self)
+  local lsx, lsy = last_x[caret_idx], last_y[caret_idx]
+  local w, h = get_caret_size(self, caret_idx)
 
-  if lsw == self and (x ~= lsx or y ~= lsy) then
+  if last_view[caret_idx] == self and x ~= lsx or y ~= lsy then
+    if not lsx then lsx = 0 end
+    if not lsy then lsy = 0 end
     local lx = x
     for i = 0, 1, 1 / config.plugins.motiontrail.steps do
-      local ix = lerp(x, lsx, i)
-      local iy = lerp(y, lsy, i)
-      if cc_conf.shape == "underline" then iy = iy + self:get_line_height() end
+      local ix = common.lerp(x, lsx, i)
+      local iy = common.lerp(y, lsy, i)
+      if cc_installed and cc_conf.shape == "underline" then
+        iy = iy + self:get_line_height()
+      end
       local iw = math.max(w, math.ceil(math.abs(ix - lx)))
-      renderer.draw_rect(ix, iy, iw, h, (cc_conf and cc_conf.custom_color) and cc_conf.caret_color or style.caret)
+      local color = style.caret
+      if cc_conf.custom_color then
+        color = cc_conf.caret_color
+      end
+      renderer.draw_rect(ix, iy, iw, h, color)
       lx = ix
     end
     core.redraw = true
   end
 
-  last_view[caret_index], last_x[caret_index], last_y[caret_index] = self, x, y
+  last_x[caret_idx], last_y[caret_idx], last_view[caret_idx] = x, y, self
 
-  caret_index = caret_index + 1
+  caret_idx = caret_idx + 1
 
-  dv_draw_caret(self, x,y)
+  dv_draw_caret(self, x, y)
 end
