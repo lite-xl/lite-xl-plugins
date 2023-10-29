d76503024e223f2cb56c29ba355d41520425f05d
ThaCuber <70547062+thacuber2a03@users.noreply.github.com>
2023-10-29 07:23:26 -0400

    Add `language_c7` and update `language_fe` (#328)
    * update lang_fe and add lang_c7
    
    * actually add the files
    
    * fixed language_fe
    
    * add to meta_languages and bump
    
    * space
    
    * .*

--- plugins/language_c7.lua
+++ plugins/language_c7.lua
@@ -2,12 +2,15 @@
 local syntax = require "core.syntax"
 
 syntax.add {
-  name = "fe",
-  files = "%.fe$",
+  name = "cel7",
+  files = "%.c7$",
   comment = ";",
   patterns = {
     { pattern = ";.*",                type = "comment"  },
     { pattern = { '"', '"', '\\' },   type = "string"   },
+    { pattern = "0x4000",             type = "literal"  },
+    { pattern = "0x4040",             type = "literal"  },
+    { pattern = "0x52a0",             type = "literal"  },
     { pattern = "0x[%da-fA-F]+",      type = "number"   },
     { pattern = "-?%d+[%d%.]*",       type = "number"   },
     { pattern = "-?%.?%d+",           type = "number"   },
@@ -15,11 +18,14 @@ syntax.add {
     { pattern = "=",                  type = "symbol"   },
     { pattern = "<=?",                type = "symbol"   },
     { pattern = "[%+-%*/]",           type = "symbol"   },
+    { pattern = "//",                 type = "keyword2" },
+    { pattern = "%%",                 type = "keyword2" },
     { pattern = "%f[^(][^()'%s\"]+",  type = "function" },
     { pattern = "[^()'%s\"]+",        type = "symbol"   },
   },
   symbols = {
     ["let"]    = "keyword",
+    ["="]      = "operator",
     ["if"]     = "keyword",
     ["fn"]     = "keyword",
     ["mac"]    = "keyword",
@@ -48,5 +54,27 @@ syntax.add {
     ["/"]      = "operator",
     ["nil"]    = "literal",
     ["t"]      = "literal",
+
+    -- reserved variables (config)
+    ["title"]  = "keyword2",
+    ["width"]  = "keyword2",
+    ["height"] = "keyword2",
+    ["debug"]  = "keyword2",
+
+    -- callbacks
+    ["init"]    = "keyword2",
+    ["step"]    = "keyword2",
+    ["keydown"] = "keyword2",
+    ["keyup"]   = "keyword2",
+
+    -- built-in functions
+    ["quit"]  = "keyword2",
+    ["rand"]  = "keyword2",
+    ["poke"]  = "keyword2",
+    ["peek"]  = "keyword2",
+    ["color"] = "keyword2",
+    ["put"]   = "keyword2",
+    ["get"]   = "keyword2",
+    ["fill"]  = "keyword2",
   }
 }
