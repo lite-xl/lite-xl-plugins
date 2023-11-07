557c4d89d07685565652ca5245b0a52516894c57
devrandom <125209101+c6c534e825f4a3d41ede3e67473187d1@users.noreply.github.com>
2023-11-07 15:14:46 +0000

    added cjson comment support

--- plugins/language_json.lua
+++ plugins/language_json.lua
@@ -4,9 +4,14 @@ local syntax = require "core.syntax"
 
 syntax.add {
   name = "JSON",
-  files = { "%.json$" },
+  files = { "%.json$", "%.cjson$" },
   comment = nil,
   patterns = {
+  
+    -- cjson support
+    { pattern = "//.-\n", type = "comment" },
+    { pattern = { "/%*", "%*/" }, type = "comment" },
+    
     { pattern = '"[^\n]*"%s*:', type = "keyword" }, -- key
     { pattern = '"[^\n]*"', type = "string" }, -- value
     { pattern = "0x[%da-fA-F]+", type = "number" },
