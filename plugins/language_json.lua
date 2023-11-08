b346f7751ce279179f8ad8ae0def34eb7ccedf3d
devrandom <125209101+c6c534e825f4a3d41ede3e67473187d1@users.noreply.github.com>
2023-11-08 09:35:52 +0100

    avoid highlighting colon

--- plugins/language_json.lua
+++ plugins/language_json.lua
@@ -13,7 +13,7 @@ syntax.add {
     { pattern = "//.*", type = "comment" },
     { pattern = { "/%*", "%*/" }, type = "comment" },
     
-    { pattern = '"[^\n]-"%s*:', type = "keyword" }, -- key
+    { pattern = '"[^\n]-"()%s*:', type = "normal" }, -- key
     { pattern = '"[^\n]-"', type = "string" }, -- value
     { pattern = "0x[%da-fA-F]+", type = "number" },
     { pattern = "-?%d+[%d%.eE]*",  type = "number" },
