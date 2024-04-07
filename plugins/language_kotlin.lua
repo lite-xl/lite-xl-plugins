681511d4389b2153ba6358d4682c583a26eadd05
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-04-07 21:24:12 +0200

    Fixed types indentation.

--- plugins/language_kotlin.lua
+++ plugins/language_kotlin.lua
@@ -20,12 +20,12 @@ syntax.add {
     { pattern = "-?0x%x+",                     type = "number"   }, -- ?
     { pattern = "-?%d+[%deE]*f?",              type = "number"   }, -- ?
     { pattern = "-?%.?%d+f?",                  type = "number"   }, -- ?
-    { regex = [[\-\>(?=\s)]],                  type = "operator" },  -- Lambda
+    { regex = [[\-\>(?=\s)]],                  type = "operator" }, -- Lambda
     { regex = [[\.{2}\<?\s?(?=[a-z0-9])]],     type = "operator" }, -- Range operators
     { pattern = "[%+%-=/%*%^%%<>!~|&]",        type = "operator" }, -- Operators
     { pattern = "[%a_][%w_]*%f[(]",            type = "function" }, -- Function/Method/Class
-    { regex = [[\@[a-zA-Z]+]],                  type = "string" },   -- Annotations
-    { regex = [[this(?=\.)]],                   type = "keyword" },  -- this keyword
+    { regex = [[\@[a-zA-Z]+]],                 type = "string" },   -- Annotations
+    { regex = [[this(?=\.)]],                  type = "keyword" },  -- this keyword
     { regex = "[A-Z]+_?[A-Z]+",                type = "keyword2" }, -- Constants, FULL UPPERCASE
   },
   symbols = {
