0fd88dabcb80ead6599ba5cbbe32ffeddb956c06
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-04-17 21:05:37 +0200

    Added some patterns and all (?) keywords.

--- plugins/language_ocaml.lua
+++ plugins/language_ocaml.lua
@@ -4,17 +4,75 @@ local syntax = require "core.syntax"
 syntax.add {
   name = "OCaml",
   files = { "%.ml$" },
-  comment = "",
+  comment = "%(%*",
+  block_comment = { "%(%*", "%*%)" },
   patterns = {
-    { pattern = "%-%-.*",               type = "comment"  }, -- ?
-    { pattern = { "{%-", "%-}" },       type = "comment"  }, -- ?
-    { pattern = { '"', '"', '\\' },     type = "string"   }, -- ?
-    { pattern = { "'", "'", '\\' },     type = "string"   }, -- ?
-    { pattern = "-?0x%x+",              type = "number"   }, -- ?
-    { pattern = "-?%d+[%d%.eE]*f?",     type = "number"   }, -- ?
-    { pattern = "-?%.?%d+f?",           type = "number"   }, -- ?
+    { pattern = "%(%*.*",                     type = "comment"  }, -- Single-line comment
+    { pattern = { "%(%*", "%*%)", "%*" },     type = "comment"  }, -- Multi-line comment
+    { pattern = { '"', '"', '\\' },           type = "string"   }, -- String
+    { pattern = "-?0x%x+",                    type = "number"   }, -- ?
+    { pattern = "-?%d+[%d%.eE]*f?",           type = "number"   }, -- ?
+    { pattern = "-?%.?%d+f?",                 type = "number"   }, -- ?
+    -- TODO: function
   },
   symbols = {
-    [""] = "keyword",
+    ["and"] = "keyword",
+    ["as"] = "keyword",
+    ["asr"] = "keyword",
+    ["assert"] = "keyword",
+    ["begin"] = "keyword",
+    ["class"] = "keyword",
+    ["constraint"] = "keyword",
+    ["do"] = "keyword",
+    ["done"] = "keyword",
+    ["downto"] = "keyword",
+    ["else"] = "keyword",
+    ["end"] = "keyword",
+    ["exception"] = "keyword",
+    ["external"] = "keyword",
+    ["false"] = "keyword",
+    ["for"] = "keyword",
+    ["fun"] = "keyword",
+    ["function"] = "keyword",
+    ["functor"] = "keyword",
+    ["if"] = "keyword",
+    ["in"] = "keyword",
+    ["include"] = "keyword",
+    ["inherit"] = "keyword",
+    ["initializer"] = "keyword",
+    ["land"] = "keyword",
+    ["lazy"] = "keyword",
+    ["let"] = "keyword",
+    ["lor"] = "keyword",
+    ["lsl"] = "keyword",
+    ["lsr"] = "keyword",
+    ["lxor"] = "keyword",
+    ["match"] = "keyword",
+    ["method"] = "keyword",
+    ["mod"] = "keyword",
+    ["module"] = "keyword",
+    ["open"] = "keyword",
+    ["mutable"] = "keyword",
+    ["new"] = "keyword",
+    ["nonrec"] = "keyword",
+    ["object"] = "keyword",
+    ["of"] = "keyword",
+    ["open"] = "keyword",
+    ["open!"] = "keyword",
+    ["or"] = "keyword",
+    ["private"] = "keyword",
+    ["rec"] = "keyword",
+    ["sig"] = "keyword",
+    ["struct"] = "keyword",
+    ["then"] = "keyword",
+    ["to"] = "keyword",
+    ["true"] = "keyword",
+    ["try"] = "keyword",
+    ["type"] = "keyword",
+    ["val"] = "keyword",
+    ["virtual"] = "keyword",
+    ["when"] = "keyword",
+    ["while"] = "keyword",
+    ["with"] = "keyword",
   },
 }
