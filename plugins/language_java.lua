aba7f5a2ac2d1b329f23dabfe2989fa153649c7b
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-04-21 20:14:17 +0200

    Fixed class name pattern and tidied up some things.

--- plugins/language_java.lua
+++ plugins/language_java.lua
@@ -6,21 +6,21 @@ syntax.add {
   files = { "%.java$" },
   comment = "//",
   patterns = {
-    { pattern = "//.-\n",               type = "comment"  },
-    { pattern = { "/%*", "%*/" },       type = "comment"  },
-    { pattern = { '"', '"', '\\' },     type = "string"   },
-    { pattern = { "'", "'", '\\' },     type = "string"   },
-    { pattern = "'\\x%x?%x?%x?%x'",     type = "string"   }, -- character hexadecimal escape sequence
-    { pattern = "'\\u%x%x%x%x'",        type = "string"   }, -- character unicode escape sequence
-    { pattern = "'\\?.'",               type = "string"   }, -- character literal
-    { pattern = "-?0x%x+",              type = "number"   },
-    { pattern = "-?%d+[%d%.eE]*f?",     type = "number"   },
-    { pattern = "-?%.?%d+f?",           type = "number"   },
-    { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" },
-    { pattern = "[%a_][%w_]*%f[(]",     type = "function" },
-    { regex = "[A-Z][A-Z_]*",           type = "keyword2" }, -- Constants
-    { pattern = "[%a_][%w_]*",          type = "symbol"   }
-    --{ regex = "(?<=\s)([A-Z]+[a-z]+)+(?=\s[a-z])", type = "function" }, -- Class name in class instance
+    { pattern = "//.-\n",                            type = "comment"  },
+    { pattern = { "/%*", "%*/" },                    type = "comment"  },
+    { pattern = { '"', '"', '\\' },                  type = "string"   },
+    { pattern = { "'", "'", '\\' },                  type = "string"   },
+    { pattern = "'\\x%x?%x?%x?%x'",                  type = "string"   }, -- character hexadecimal escape sequence
+    { pattern = "'\\u%x%x%x%x'",                     type = "string"   }, -- character unicode escape sequence
+    { pattern = "'\\?.'",                            type = "string"   }, -- character literal
+    { pattern = "-?0x%x+",                           type = "number"   },
+    { pattern = "-?%d+[%d%.eE]*f?",                  type = "number"   },
+    { pattern = "-?%.?%d+f?",                        type = "number"   },
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",              type = "operator" },
+    { pattern = "[%a_][%w_]*%f[(]",                  type = "function" },
+    { regex   = "[A-Za-z]+(?=\\s[a-z0-9]+\\s\\=\\s[new])",   type = "function" }, -- Class name in class instance
+    { regex   = "[A-Z][A-Z_]+",                      type = "keyword2" }, -- Constants
+    { pattern = "[%a_][%w_]*",                       type = "symbol"   },
   },
   symbols = {
     ["abstract"]      = "keyword",
@@ -82,6 +82,6 @@ syntax.add {
 
     ["true"]     = "literal",
     ["false"]    = "literal",
-    ["null"]     = "literal",
-  },
+    ["null"]     = "literal"
+  }
 }
