224603a74905b774821cf8ec3714e30d0c46189f
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-04-24 21:39:55 +0200

    Updated some patterns.

--- plugins/language_erlang.lua
+++ plugins/language_erlang.lua
@@ -7,15 +7,16 @@ syntax.add {
   comment = "%%",
   patterns = {
     { pattern = "%%.*",                                     type = "comment"  }, -- Single-line comment
+    { regex   = "^\\w+(?!.+)",                              type = "literal"  }, -- Atom
+    { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",              type = "literal"  }, -- Atom with apices
     { pattern = { '"', '"', '\\' },                         type = "string"   }, -- String, quotes
+    { pattern = { "'", "'", '\\' },                         type = "string"   }, -- String, apices
     { regex   = "\\w+[0-9]*(?=[)])",                        type = "symbol"   }, -- Field name
     { pattern = "-?0x%x+",                                  type = "number"   }, -- ?
     { pattern = "-?%d+[%deE]*f?",                           type = "number"   }, -- ?
     { pattern = "-?%.?%d+f?",                               type = "number"   }, -- ?
     { regex   = [[\-\>(?=\s)]],                             type = "function" }, -- Function arrow
     { pattern = "-?[%a_][%w_]*%f[(]",                       type = "function" }, -- Function name
-    { regex   = "^\\w+",                                    type = "keyword2" }, -- Atom
-    { regex   = "^\\'?\\w+\\s?\\w+\\'?",                    type = "keyword2" }, -- Atom
     { regex   = "<<.+>>",                                   type = "keyword2" }, -- bit string
     { regex   = "\\#\\{.+\\}",                              type = "keyword2" }, -- map
     { pattern = "[%+%-=/%*%^<>!~|&]",                       type = "operator" }, -- Operators
