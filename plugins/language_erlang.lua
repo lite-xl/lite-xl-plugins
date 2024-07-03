328f99120f7bf04e59881b1930d70011562133d1
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-07-03 10:55:55 +0200

    WIP: reworking some things.

--- plugins/language_erlang.lua
+++ plugins/language_erlang.lua
@@ -6,38 +6,36 @@ syntax.add {
   files = "%.erl$", "%.hrl$",
   comment = "%%",
   patterns = {
-    { pattern = "%%.*",                                     type = "comment"  }, -- Single-line comment
-    { regex   = "^\\w+(?!.+)",                              type = "literal"  }, -- Atom
-    { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",              type = "literal"  }, -- Atom with apices
-    { pattern = { '"', '"', '\\' },                         type = "string"   }, -- String, quotes
-    { pattern = { "'", "'", '\\' },                         type = "string"   }, -- String, apices
-    { regex   = "\\w+[0-9]*(?=[)])",                        type = "symbol"   }, -- Field name
-    { pattern = "-?0x%x+",                                  type = "number"   }, -- ?
-    { pattern = "-?%d+[%deE]*f?",                           type = "number"   }, -- ?
-    { pattern = "-?%.?%d+f?",                               type = "number"   }, -- ?
-    { regex   = "\\$\\w+(?=\\s?\\=?)",                      type = "keyword2" }, -- Type
-    { regex   = "[a-zA-Z0-9_]+\\#[a-zA-Z0-9_]+",            type = "keyword2" }, -- Type
-    { regex   = "^\\-\\w+",                                 type = "keyword"  }, -- Modules
-    { regex   = [[\-\>(?=\s)]],                             type = "function" }, -- Function arrow
-    { pattern = "-?[%a_][%w_]*%f[(]",                       type = "function" }, -- Function name
-    { regex   = "^\\w+(?!.+)",                              type = "keyword2" }, -- Atom
-    { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",              type = "keyword2" }, -- Atom
-    { regex   = "<<.+>>",                                   type = "keyword2" }, -- Bit string
-    { regex   = "\\#\\w*\\{.*\\}(?![\\s\\=\\s\\#])",        type = "keyword2" }, -- Map
-    { pattern = "[%+%-=/%*%^<>!~|&?]",                      type = "operator" }, -- Operators
-    { regex   = "bnot|div|rem|band|bor|bxor|bsl|bsr",       type = "operator" }, -- Operators
+    { pattern = "%%.*",                                             type = "comment"  }, -- Single-line comment
+    { regex   = "^\\w+(?!.+)",                                      type = "literal"  }, -- Atom
+    { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",                      type = "literal"  }, -- Atom with apices
+    { pattern = { '"', '"', '\\' },                                 type = "string"   }, -- String, quotes
+    { pattern = { "'", "'", '\\' },                                 type = "string"   }, -- String, apices
+    { regex   = "\\w+[0-9]*(?=[)])",                                type = "symbol"   }, -- Field name
+    { pattern = "-?0x%x+",                                          type = "number"   }, -- ?
+    { pattern = "-?%d+[%deE]*f?",                                   type = "number"   }, -- ?
+    { pattern = "-?%.?%d+f?",                                       type = "number"   }, -- ?
+    { regex   = "\\$\\w+(?=\\s?\\=?)",                              type = "keyword2" }, -- Type
+    { regex   = "[a-zA-Z0-9_]+\\#[a-zA-Z0-9_]+",                    type = "keyword2" }, -- Type
+    { regex   = "^\\-\\w+",                                         type = "keyword"  }, -- Modules
+    { regex   = [[\-\>(?=\s)]],                                     type = "function" }, -- Function arrow
+    { pattern = "-?[%a_][%w_]*%f[(]",                               type = "function" }, -- Function name
+    { regex   = "^\\w+(?!.+)",                                      type = "keyword2" }, -- Atom
+    { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",                      type = "keyword2" }, -- Atom
+    { regex   = "<<.+>>",                                           type = "keyword2" }, -- Bit string
+    { pattern = "[%+%-=/%*%^<>!~|&?]",                              type = "operator" }, -- Operators
+    { regex   = "bnot|div|rem|band|bor|bxor|bsl|bsr",               type = "operator" }, -- Operators
     -- TODO: add missing number expressions
-    -- FIXME: true or false, true is colored like an atom
-    -- FIXME: fix maps coloring on same line
-    -- FIXME: try should be colored as keyword
+    -- FIXME: fix number coloring in field names
     -- FIXME: add missing number formats
+    -- FIXME: true or false, true is colored like an atom
+    -- FIXME: try should be colored as keyword
     -- TODO: add support for records
     -- TODO: color rec#{...}, set#{...} and similar
-    -- FIXME: fix number coloring in field names
     -- TODO: add support for nested records
     -- TODO: add support for macros
   },
-  symbols = {    
+  symbols = {
     ["after"]         = "keyword",
     ["and"]           = "keyword",
     ["andalso"]       = "keyword",
