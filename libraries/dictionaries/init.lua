1b45ffc64ed1e633c8aa9729b71bf94eaaaaa5cd
Gaspartcho <93390411+Gaspartcho@users.noreply.github.com>
2024-07-03 09:57:26 +0000

    Update init.lua

--- libraries/dictionaries/init.lua
+++ libraries/dictionaries/init.lua
@@ -3,10 +3,13 @@ local core = require "core"
 
 local dict = {languages = {}}
 
+local ok, t
+
 
 -- English
 -- Source: https://github.com/fordsfords/moby_words_2/blob/main/crosswd.txt
-local ok, t = pcall(require, "libraries.dictionaries.words-EN")
+-- Source 2: https://www.freelang.com/download/misc/liste_francais.zip
+ok, t = pcall(require, "libraries.dictionaries.words-EN")
 if ok then
 	dict["EN"] = t
 	table.insert(dict.languages, "EN")
@@ -17,6 +20,13 @@ end
 
 -- French
 -- Source: https://github.com/Taknok/French-Wordlist/blob/master/francais.txt
-
+ok, t = pcall(require, "libraries.dictionaries.words-FR")
+if ok then
+	dict["FR"] = t
+	table.insert(dict.languages, "FR")
+	core.log("[Dictionaries]: Successfully loaded the English dictionary")
+else
+	core.error("[Dictionaries]: Failed to load the English dictionary")
+end
 
 return dict
