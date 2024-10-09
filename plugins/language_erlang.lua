3702f7c3762c6aa37927dac7d98d211883de0132
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-10-09 19:00:41 +0200

    WIP: ...

--- plugins/language_erlang.lua
+++ plugins/language_erlang.lua
@@ -1,10 +1,9 @@
 -- mod-version:3
 local syntax = require "core.syntax"
 
+-- Language Syntax References
 -- https://www.erlang.org/doc/system/reference_manual.html
 -- https://erlang.org/documentation/doc-5.6/pdf/reference_manual.pdf
-
--- WIP: https://www.erlang.org/doc/system/ref_man_records.html#updating-records
 
 syntax.add {
   name = "Erlang",
@@ -14,15 +13,17 @@ syntax.add {
     { pattern = "%%.*",                                             type = "comment"  }, -- Single-line comment
     { regex   = "^\\w+(?!.+)",                                      type = "literal"  }, -- Atom
     { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",                      type = "literal"  }, -- Atom with apices
+    { regex   = "^\\w+\\@\\w+",                                     type = "literal"  }, -- Atom with @
     { pattern = { '"', '"', '\\' },                                 type = "string"   }, -- String, quotes
     { pattern = { "'", "'", '\\' },                                 type = "string"   }, -- String, apices
     { regex   = "\\w+[0-9]*(?=[)])",                                type = "symbol"   }, -- Field name
-    { pattern = "-?0x%x+",                                          type = "number"   }, -- ?
-    { pattern = "-?%d+[%deE]*f?",                                   type = "number"   }, -- ?
-    { pattern = "-?%.?%d+f?",                                       type = "number"   }, -- ?
+    { regex   = "^\\$\\\\n",                                        type = "number"   }, -- Numbers, $\n
+    { regex   = "^\\$(?:\\w|\\n)",                                  type = "number"   }, -- Numbers, $char
+    { regex   = "(?:\\d_?)+\\#(?:\\w)+",                            type = "number"   }, -- Numbers, base#value
+    { regex   = "[+|-]?(?:\\d_?)+[eE]?(?:.\\d+[eE]?_?\\d*)?",       type = "number"   }, -- Numbers
     { regex   = "\\$\\w+(?=\\s?\\=?)()\n",                          type = { "keyword2", "literal" } }, -- Type
     { regex   = "[a-zA-Z0-9_]+()\\#[a-zA-Z0-9_]+",                  type = { "function", "keyword2" } }, -- Updating records
-    { regex   = "\\#[a-zA-Z0-9_]+",                                 type = "keyword2" }, -- Records
+    { regex   = "\\#\\w*",                                          type = "keyword2" }, -- Records
     { regex   = "^\\-\\w+",                                         type = "keyword"  }, -- Modules
     { regex   = [[\-\>(?=\s)]],                                     type = "function" }, -- Function arrow
     { pattern = "-?[%a_][%w_]*%f[(]",                               type = "function" }, -- Function name
@@ -30,18 +31,12 @@ syntax.add {
     { regex   = "^\\'?\\w+\\s?\\w+\\'?(?!.+)",                      type = "keyword2" }, -- Atom
     { regex   = "<<.+>>",                                           type = "keyword2" }, -- Bit string
     { pattern = "[%+%-=/%*%^<>!~|&?]",                              type = "operator" }, -- Operators
-    { regex   = "bnot|div|rem|band|bor|bxor|bsl|bsr",               type = "operator" }, -- Operators
-    -- TODO: symbols pattern
-    -- TODO: add missing number expressions
-    -- FIX: add missing number formats
+    { regex   = "^\\-doc()\\s\\~.+",                                type = { "keyword", "string" } }, -- Docs
+    { pattern = "[%a_][%w_]*",                                      type = "symbol"   }, -- Symbols
     -- FIX: fix number coloring in field names
-    -- FIX: true or false, true is colored like an atom
-    -- FIX: try should be colored as keyword
-    -- TODO: color rec#{...}, set#{...} and similar
+    -- FIX: ~S/Example "docs"/ and ~B|Example "docs"| should be colored as strings
+    -- FIX: number strings inside multi-line """ ... """ (requires subsyntax)
     -- TODO: add support for macros
-    -- FIX: number strings inside multi-line """ ... """
-    -- FIX: ~S/Example "docs"/ and ~B|Example "docs"| should be colored as string
-    -- FIX: end.
   },
   symbols = {
     ["after"]         = "keyword",
