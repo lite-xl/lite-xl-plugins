127edd2d1a05fbe765785948428b074c9ddbb5cc
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-11-04 12:12:02 +0100

    Add support for Plank's config and theme files.

--- plugins/language_ini.lua
+++ plugins/language_ini.lua
@@ -4,7 +4,7 @@ local syntax = require "core.syntax"
 
 syntax.add {
   name = "INI",
-  files = { "%.ini$", "%.inf$", "%.cfg$", "%.editorconfig$" },
+  files = { "%.ini$", "%.inf$", "%.cfg$", "%.editorconfig$", "%.theme$", "%.dockitem$" },
   comment = ';',
   patterns = {
     { pattern = ";.-\n", type = "comment" },
