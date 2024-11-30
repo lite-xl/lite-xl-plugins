e9e0f952e0c84bb332873e41a12a103808e557b5
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-11-30 12:39:31 +0100

    WIP: updating Java syntax.

--- plugins/language_java.lua
+++ plugins/language_java.lua
@@ -1,26 +1,35 @@
 -- mod-version:3
 local syntax = require "core.syntax"
 
+-- Language Syntax References
+-- https://docs.oracle.com/javase/specs/jls/se8/html/index.html
+
 syntax.add {
   name = "Java",
   files = { "%.java$" },
   comment = "//",
   patterns = {
-    { pattern = "//.*",                                                              type = "comment"  },
-    { pattern = { "/%*", "%*/" },                                                    type = "comment"  },
-    { pattern = { '"', '"', '\\' },                                                  type = "string"   },
-    { pattern = { "'", "'", '\\' },                                                  type = "string"   },
-    { pattern = "'\\x%x?%x?%x?%x'",                                                  type = "string"   }, -- character hexadecimal escape sequence
-    { pattern = "'\\u%x%x%x%x'",                                                     type = "string"   }, -- character unicode escape sequence
-    { pattern = "'\\?.'",                                                            type = "string"   }, -- character literal
-    { pattern = "-?0x%x+",                                                           type = "number"   },
-    { pattern = "-?%d+[%d%.eE]*f?",                                                  type = "number"   },
-    { pattern = "-?%.?%d+f?",                                                        type = "number"   },
-    { pattern = "[%+%-=/%*%^%%<>!~|&]",                                              type = "operator" },
-    { pattern = "[%a_][%w_]*%f[(]",                                                  type = "function" },
-    { regex   = "(?>\\w+\\.?)+(?=\\s+\\w++\\s*\\=\\s*)",                             type = "function" }, -- Class name when creating an object
-    { regex   = "[A-Z](?:[A-Z_][\\d]*)+(?=\\s*[)]|[\\;]|[\\,]|[\\s\\=]|[\\.])",      type = "keyword2" }, -- Constants
-    { pattern = "[%a_][%w_]*",                                                       type = "symbol"   },
+    { pattern = "//.*",                                       type = "comment"  },
+    { pattern = { "/%*", "%*/" },                             type = "comment"  },
+    { pattern = { '"', '"', '\\' },                           type = "string"   },
+    { pattern = { "'", "'", '\\' },                           type = "string"   },
+    { pattern = "'\\x%x?%x?%x?%x'",                           type = "string"   }, -- character hexadecimal escape sequence
+    { pattern = "'\\u%x%x%x%x'",                              type = "string"   }, -- character unicode escape sequence
+    { pattern = "'\\?.'",                                     type = "string"   }, -- character literal
+    { pattern = "-?0x%x+",                                    type = "number"   },
+    { pattern = "-?%d+[%d%.eE]*f?",                           type = "number"   },
+    { pattern = "-?%.?%d+f?",                                 type = "number"   },
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",                       type = "operator" },
+    { pattern = "[%a_][%w_]*%f[(]",                           type = "function" },
+    { regex   = "(?>\\w+\\.?)+(?=\\s+\\w++\\s*\\=\\s*)",      type = "function" }, -- Class name when creating an object
+    { regex   = [[this(?=\.?\@?)]],                           type = "keyword"  }, -- this keyword
+    { regex   = "^\\@.+",                                     type = "keyword2" }, -- Annotations
+    { regex   = [[[a-zA-Z]+\@(?=\s?[a-zA-Z])]],               type = "keyword2" }, -- Annotations (this pattern is lower priority than the `this keyword` pattern)
+    { pattern = "import()%s+()[%w_.]+",                       type = { "keyword", "normal", "normal" } },
+    { regex   = "[A-Z](?:[A-Z_][\\d]*)+",                     type = "keyword2" }, -- Constants
+    { pattern = "[%a_][%w_]*",                                type = "symbol"   },
+    -- TODO: generic class instance pattern
+    -- FIX: class instance pattern like "ForgeConfigSpec.IntValue"
   },
   symbols = {
     ["abstract"]      = "keyword",
