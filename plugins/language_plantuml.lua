60d71856d46d3e38637e103bfb3db27fd5cd8be3
PerilousBooklet <raffaele.orabona@protonmail.com>
2025-01-14 19:03:23 +0100

    Fixing conflict.

--- plugins/language_plantuml.lua
+++ plugins/language_plantuml.lua
@@ -1,1 +1,21 @@
+-- mod-version:3
+local syntax = require "core.syntax"
 
+-- Language Syntax Reference
+-- https://plantuml.com/
+
+syntax.add {
+  name = "Plantuml",
+  files = "%.puml$",
+  comment = "//",
+  block_comment = {"/*", "*/"},
+  patterns = {
+    { pattern = "^%@%w+", type = "keyword" }, -- ?
+    --{ pattern = "", type = "" }, -- ?
+    { pattern = "[%+%-=/%*%^%%<>!~|&%?%:]",      type = "operator" }, -- Operators
+    { pattern = "[%a_][%w_]*",                 type = "symbol"   } -- Everything else
+  },
+  symbols = {
+    ["true"] = "literal",
+  }
+}
