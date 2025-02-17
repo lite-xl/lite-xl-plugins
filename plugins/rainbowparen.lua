a6ca33993ac1b91d864a402dfea4005c32e58012
Adam Harrison <adamdharrison@gmail.com>
2025-02-17 16:28:09 -0500

    Updated rainbowparen to use the new virutal line system.

--- plugins/rainbowparen.lua
+++ plugins/rainbowparen.lua
@@ -5,7 +5,7 @@ local config = require "core.config"
 local common = require "core.common"
 local command = require "core.command"
 local tokenizer = require "core.tokenizer"
-local Highlighter = require "core.doc.highlighter"
+local DocView = require "core.docview"
 
 config.plugins.rainbowparen = common.merge({
   enabled = true,
@@ -19,8 +19,6 @@ style.syntax.paren3  =  style.syntax.paren3 or { common.color "#fcd476"}
 style.syntax.paren4  =  style.syntax.paren4 or { common.color "#52dab2"}
 style.syntax.paren5  =  style.syntax.paren5 or { common.color "#5a98cf"}
 
-local tokenize = tokenizer.tokenize
-local extract_subsyntaxes = tokenizer.extract_subsyntaxes
 local closers = {
   ["("] = ")",
   ["["] = "]",
@@ -31,61 +29,74 @@ local function parenstyle(parenstack)
   return "paren" .. ((#parenstack % config.plugins.rainbowparen.parens) + 1)
 end
 
-function tokenizer.extract_subsyntaxes(base_syntax, state)
-  if not config.plugins.rainbowparen.enabled then
-    return extract_subsyntaxes(base_syntax, state)
-  end
-  return extract_subsyntaxes(base_syntax, state.istate)
-end
 
-function tokenizer.tokenize(syntax, text, state, resume)
+local old_tokenize = DocView.tokenize
+function DocView:tokenize(line)
   if not config.plugins.rainbowparen.enabled then
-    return tokenize(syntax, text, state, resume)
+    return old_tokenize(self, line)
   end
-  state = state or {}
-  local res, istate, resume = tokenize(syntax, text, state.istate, resume)
-  local parenstack = state.parenstack or ""
-  local newres = {}
+  if not self.parenstack then self.parenstack = {} end
+  local tokens = old_tokenize(self, line)
+  local parenstack = self.parenstack[line-1] or ""
+  local newtokens = {}
   -- split parens out
   -- the stock tokenizer can't do this because it merges identical adjacent tokens
-  for i, type, text in tokenizer.each_token(res) do
-    if type == "normal" or type == "symbol" then
+  for idx, type, doc_line, col_start, col_end, line_style in self:each_token(tokens) do
+    if type == "doc" and (line_style.type == "normal" or line_style.type == "symbol") then
+      local text = self:get_token_text(type, doc_line, col_start, col_end)
+      local offset = col_start
       for normtext1, paren, normtext2 in text:gmatch("([^%(%[{}%]%)]*)([%(%[{}%]%)]?)([^%(%[{}%]%)]*)") do
         if #normtext1 > 0 then
-          table.insert(newres, type)
-          table.insert(newres, normtext1)
+          table.insert(newtokens, "doc")
+          table.insert(newtokens, doc_line)
+          table.insert(newtokens, offset)
+          table.insert(newtokens, offset + #normtext1 - 1)
+          table.insert(newtokens, line_style)
+          offset = offset + #normtext1
         end
         if #paren > 0 then
+          table.insert(newtokens, "doc")
+          table.insert(newtokens, doc_line)
+          table.insert(newtokens, offset)
+          table.insert(newtokens, offset)
           if paren == parenstack:sub(-1) then -- expected closer
             parenstack = parenstack:sub(1, -2)
-            table.insert(newres, parenstyle(parenstack))
+            table.insert(newtokens, common.merge(line_style, { color = style.syntax[parenstyle(parenstack)] }))
           elseif closers[paren] then -- opener
-            table.insert(newres, parenstyle(parenstack))
+            table.insert(newtokens, common.merge(line_style, { color = style.syntax[parenstyle(parenstack)] }))
             parenstack = parenstack .. closers[paren]
           else -- unexpected closer
-            table.insert(newres, "paren_unbalanced")
+            table.insert(newtokens, common.merge(line_style, { color = style.syntax["paren_unbalanced"] }))
           end
-          table.insert(newres, paren)
+          offset = offset + #paren
         end
         if #normtext2 > 0 then
-          table.insert(newres, type)
-          table.insert(newres, normtext2)
+          table.insert(newtokens, "doc")
+          table.insert(newtokens, doc_line)
+          table.insert(newtokens, offset)
+          table.insert(newtokens, offset + #normtext2 - 1)
+          table.insert(newtokens, line_style)
+          offset = offset + #normtext2
         end
       end
     else
-      table.insert(newres, type)
-      table.insert(newres, text)
+      table.insert(newtokens, type)
+      table.insert(newtokens, doc_line)
+      table.insert(newtokens, col_start)
+      table.insert(newtokens, col_end)
+      table.insert(newtokens, line_style)
     end
   end
-  return newres, { parenstack = parenstack, istate = istate }, resume
+  if parenstack ~= self.parenstack[line] then
+    self.parenstack[line] = parenstack
+    if line < #self.doc.lines then self:invalidate_cache(line + 1) end
+  end
+  return newtokens
 end
 
 local function toggle_rainbowparen(enabled)
   config.plugins.rainbowparen.enabled = enabled
-  for _, doc in ipairs(core.docs) do
-    doc.highlighter = Highlighter(doc)
-    doc:reset_syntax()
-  end
+  for _, doc in ipairs(core.docs) do doc:reset_syntax() end
 end
 
 -- The config specification used by the settings gui
