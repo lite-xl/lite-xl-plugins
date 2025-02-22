cff8b9ca78c2d9f42630be4bc921620857182c30
PerilousBooklet <raffaele.orabona@protonmail.com>
2025-02-22 16:41:08 +0100

    WIP: adding patterns

--- plugins/language_plantuml.lua
+++ plugins/language_plantuml.lua
@@ -10,14 +10,59 @@ syntax.add {
   comment = "/'",
   block_comment = { "/'", "'/" },
   patterns = {
-    { pattern = "^%'.*$",                                type = "comment" }, -- Single-line comment
-    { pattern = { "^/'", "%'%/" } ,                      type = "comment" }, -- Multi-line comment
-    { pattern = "^%@%w+",                                type = "keyword2" }, -- start... end...
-    { pattern = "",                                      type = "operator" }, -- Operators
-    { pattern = "[%a_][%w_]*",                           type = "symbol"   }, -- Everything else
+    { pattern = "^%'.*$",                                              type = "comment"  }, -- Single-line comment
+    { pattern = { "^/'", "%'%/" } ,                                    type = "comment"  }, -- Multi-line comment
+    { pattern = { '"', '"' },                                          type = "string"   }, -- String
+    { pattern = "^%@%w+",                                              type = "keyword2" }, -- start... end...
+    { pattern = "%-+%|?%>*",                                           type = "operator" }, -- Arrow operator
+    { pattern = "%<?%|?%-+%*?",                                        type = "operator" }, -- Arrow operator
+    { pattern = "%.+%|?%>?",                                           type = "operator" }, -- Arrow operator
+    { pattern = "%<?%|?%.+",                                           type = "operator" }, -- Arrow operator
+    { pattern = "[%*ox$#$}%+%^]%-+",                                   type = "operator" }, -- Arrow operator
+    { pattern = "%<%<%w+%>%>",                                         type = "keyword2" }, -- Key
+    -- FIX: mustn't extend beyond eventual parenthesis
+    { pattern = "%:%s*.+$",                                            type = "keyword2" }, -- Attribute/Comment
+    { pattern = "^%!.+",                                               type = "keyword2" }, -- ?
+    -- TODO: type
+    -- TODO: accessibility modifiers
+    { pattern = "^%(%s*%)",                                            type = "keyword" }, -- Abbreviations
+    { pattern = "^%<%s*%>",                                            type = "keyword" }, -- Abbreviations
+    { pattern = "[%a_][%w_]*",                                         type = "symbol"   }, -- Everything else
   },
   symbols = {
+    -- Literals
     ["true"] = "literal",
     ["false"] = "literal",
+    -- Keywords
+    ["abstract"] = "keyword",
+    ["abstract class"] = "keyword",
+    ["annotation"] = "keyword",
+    ["circle"] = "keyword",
+    ["class"] = "keyword",
+    ["diamond"] = "keyword",
+    ["entity"] = "keyword",
+    ["enum"] = "keyword",
+    ["exception"] = "keyword",
+    ["interface"] = "keyword",
+    ["metaclass"] = "keyword",
+    ["protocol"] = "keyword",
+    ["stereotype"] = "keyword",
+    ["struct"] = "keyword",
+    ["as"] = "keyword",
+    ["package"] = "keyword",
+    ["remove"] = "keyword",
+    ["note"] = "keyword",
+    ["end"] = "keyword",
+    -- ?
+    ["to"] = "keyword2",
+    ["left"] = "keyword2",
+    ["right"] = "keyword2",
+    ["top"] = "keyword2",
+    ["bottom"] = "keyword2",
+    ["direction"] = "keyword2",
+    ["of"] = "keyword2",
+    ["set"] = "keyword2",
+    ["separator"] = "keyword2",
+    ["none"] = "keyword2",
   }
 }
