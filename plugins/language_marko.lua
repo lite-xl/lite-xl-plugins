e17f1c2bcd6f2c205c94faf62020003c9bbb9f71
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2025-03-29 02:50:48 +0530

    Update language_marko.lua

--- plugins/language_marko.lua
+++ plugins/language_marko.lua
@@ -1,66 +1,40 @@
--- Made by: https://github.com/RohanVashisht1234
-
 -- mod-version:3
-
--- Language Syntax Reference
--- https://markojs.com/docs/syntax/
-
--- Syntax definition for Marko.js
-
 local syntax = require "core.syntax"
 
+-- Language Syntax References
+-- https://markojs.com/docs/syntax/
+
 syntax.add {
-    name = "Marko",
+    name = "Marko JS",
     files = { "%.marko$" },
     comment = "//",
+    block_comment = { "<!%-%-", "%-%->" },
     patterns = {
-        -- Important
-        { pattern = { "%-%-\n", "%-%-" },   type = "string" }, -- Multiline string
-        { pattern = "%-%-.-\n",             type = "string" }, -- Single line string
-        { pattern = "^%s*%a[%w%-_]*",       type = "keyword" },
-
-        -- Comments
-        { pattern = "//.-\n",               type = "comment" },
-        { pattern = { "/%*", "%*/" },       type = "comment" },
-        { pattern = { "<!%-%-", "%-%->" },  type = "comment" },
-
-
-
-        -- CDATA
-        { pattern = "<!%a[%w%-_%s*]*>",     type = "comment" },
-
-        -- Strings
-        { pattern = { '"', '"', '\\' },     type = "string" },
-        { pattern = { "'", "'", '\\' },     type = "string" },
-
-        -- Numbers
-        { pattern = "0x[%da-fA-F]+",        type = "number" },
-        { pattern = "-?%d+[%d%.eE]*",       type = "number" },
-        { pattern = "-?%.?%d+",             type = "number" },
-
-        -- Operators
-        { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" },
-        { pattern = "^[$]",                 type = "keyword2" },
-
-
-        -- Marko-specific patterns
-        { pattern = "<%a[%w%-_]*",          type = "keyword" },
-
-        { pattern = "</%a[%w%-_]*>",        type = "keyword" },
-        { pattern = "${%a[%w%-_%.]*}",      type = "keyword2" },
-        { pattern = "/>",                   type = "keyword" },
-        { pattern = "%[",                   type = "keyword" },
-        { pattern = "%]",                   type = "keyword" },
-
-        -- Attributes
-        { pattern = "[%a_][%w%-_]*%s*=",    type = "keyword2" },
-
-        -- Functions and symbols
-        { pattern = "[%a_][%w_]*%f[(]",     type = "function" },
-        { pattern = "[%a_][%w%-_]*",        type = "symbol" },
+        { pattern = { "%-%-\n", "%-%-" },   type = "string" },   -- Multiline string
+        { pattern = "%-%-.-\n",             type = "string" },   -- Single line string
+        { pattern = "^%s*%a[%w%-_]*",       type = "keyword" },  -- Concise formatting keywords
+        { pattern = "//.*",                 type = "comment" },  -- Single line js/ts based comments
+        { pattern = { "/%*", "%*/" },       type = "comment" },  -- Multiline js/ts based comments
+        { pattern = { "<!%-%-", "%-%->" },  type = "comment" },  -- Multiline Marko/HTML based comments
+        { pattern = "<!%a[%w%-_%s*]*>",     type = "comment" },  -- CDATA like <!doctype html>
+        { pattern = { '"', '"', '\\' },     type = "string" },   -- String for attribute and js/ts based syntax
+        { pattern = { "'", "'", '\\' },     type = "string" },   -- String for attribute and js/ts based syntax
+        { pattern = "0x[%da-fA-F]+",        type = "number" },   -- Number
+        { pattern = "-?%d+[%d%.eE]*",       type = "number" },   -- Number
+        { pattern = "-?%.?%d+",             type = "number" },   -- Number
+        { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" }, -- Operators
+        { pattern = "^[$]",                 type = "keyword2" }, -- for $ declaration at start of line
+        { pattern = "<%a[%w%-_]*",          type = "keyword" },  -- HTML opening tags like syntax Marko
+        { pattern = "</%a[%w%-_]*>",        type = "keyword" },  -- HTML closing tags like syntax Marko
+        { pattern = "${%a[%w%-_%.]*}",      type = "keyword2" }, -- line insertion markojs
+        { pattern = "$!{%a[%w%-_%.]*}",     type = "keyword2" }, -- unsafe line insertion markojs
+        { pattern = "/>",                   type = "keyword" },  -- closing tag for multi line based html opening tag
+        { pattern = "%[",                   type = "keyword" },  -- opening tag for multi line based marko consice syntax
+        { pattern = "%]",                   type = "keyword" },  -- closing tag for multi line based marko consice syntax
+        { pattern = "[%a_][%w_]*%f[(]",     type = "function" }, -- JS/TS based function declarations
+        { pattern = "[%a_][%w%-_]*",        type = "symbol" },   -- Symbol
     },
     symbols = {
-
         ["class"]           = "keyword2",
         ["style"]           = "keyword2",
         ["src"]             = "keyword2",
