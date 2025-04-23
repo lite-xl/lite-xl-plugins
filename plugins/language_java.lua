b63d5a542da2e45d9def269d4f2c311d2aa2928c
PerilousBooklet <raffaele.orabona@protonmail.com>
2025-04-23 16:50:14 +0200

    WIP: reworked class reference patterns, only 2 pattern fixes remaining.

--- plugins/language_java.lua
+++ plugins/language_java.lua
@@ -9,30 +9,62 @@ syntax.add {
   files = { "%.java$" },
   comment = "//",
   patterns = {
-    { pattern = "//.*",                                                                   type = "comment"  },
-    { pattern = { "/%*", "%*/" },                                                         type = "comment"  },
-    { pattern = { '"', '"', '\\' },                                                       type = "string"   },
-    { pattern = { "'", "'", '\\' },                                                       type = "string"   },
-    { pattern = "'\\x%x?%x?%x?%x'",                                                       type = "string"   }, -- character hexadecimal escape sequence
-    { pattern = "'\\u%x%x%x%x'",                                                          type = "string"   }, -- character unicode escape sequence
-    { pattern = "'\\?.'",                                                                 type = "string"   }, -- character literal
-    { pattern = "-?0x%x+",                                                                type = "number"   },
-    { pattern = "-?%d+[%d%.eE]*f?",                                                       type = "number"   },
-    { pattern = "-?%.?%d+f?",                                                             type = "number"   },
-    { pattern = "%<.+%>",                                                                 type = "keyword2" }, -- Generic type
-    { pattern = "[%+%-=/%*%^%%<>!~|&]",                                                   type = "operator" },
-    { pattern = "[%a_][%w_]*%f[(]",                                                       type = "function" },
-    { pattern = "^import()%s+()[%w_.]+",                                                  type = { "keyword", "normal", "normal" } }, -- Import
-    -- FIX: see ./examples/example.java
-    { regex   = [[(?>(?>[A-Z]\w+_?\.?)+(?>\<.+\>(?=\s+))?)+(?=(?>\s+\w+)|[(])]],          type = "function" }, -- Class name reference
-    { regex   = [[this(?=\.?\@?)]],                                                       type = "keyword"  }, -- this keyword
-    { pattern = "^%s*%@.+%)",                                                             type = "keyword2" }, -- Annotation (at line start)
-    { regex   = [[\s*\@.+\)(?=\s+\w+)]],                                                  type = "keyword2" }, -- Annotation (at line middle)
-    { pattern = "%@%w+",                                                                  type = "keyword2" }, -- Annotation (like: final @Nullable String something;)
-    { pattern = "[A-Z][A-Z_%d]+%f[^a-zA-Z_%d]",                                           type = "keyword2" }, -- Constants
-    { pattern = "%:%:()%w+",                                                              type = { "normal", "function" } }, -- Method reference with double colon operator
-    { pattern = "%.class",                                                                type = "normal"   }, -- .class should be colored as normal
-    { pattern = "[%a_][%w_]*",                                                            type = "symbol"   },
+    { pattern = "//.*",                                       type = "comment"  }, -- Single-line comment
+    { pattern = { "/%*", "%*/" },                             type = "comment"  }, -- Multi-line comment
+    { pattern = { '"', '"', '\\' },                           type = "string"   }, -- String
+    { pattern = { "'", "'", '\\' },                           type = "string"   }, -- String
+    { pattern = "'\\x%x?%x?%x?%x'",                           type = "string"   }, -- character hexadecimal escape sequence
+    { pattern = "'\\u%x%x%x%x'",                              type = "string"   }, -- character unicode escape sequence
+    { pattern = "'\\?.'",                                     type = "string"   }, -- character literal
+    { pattern = "-?0x%x+",                                    type = "number"   }, -- Number, hexadecimal
+    { pattern = "-?%d+[%d%.eE]*f?",                           type = "number"   }, -- Number, exponential
+    { pattern = "-?%.?%d+f?",                                 type = "number"   }, -- Number
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",                       type = "operator" }, -- Operator
+    { pattern = "[%a_][%w_]*%f[(]",                           type = "function" }, -- Method
+    { pattern = "^import()%s+()[%w_.]+",                      type = { "keyword", "normal", "normal" } }, -- Import
+    -- Class name reference, ;
+    { pattern = "^%w+%s*()%w+%s*%;",                          type = { "function", "normal" } },
+    { pattern = "^%w+%s*()%<.*%>()%s*%w+%s*%;",               type = { "function", "keyword2", "normal" } },
+    -- Class name reference, =
+    { pattern = "%w+%s*()%w+%s*()%=",                         type = { "function", "normal", "operator" } },
+    { pattern = "%w+%s*()%<.*%>()%s*%w+%s*()%=",              type = { "function", "keyword2", "normal", "operator" } },
+    -- Class name reference, new
+    { pattern = "new()%s*%w+()%<.*%>()%f[(]",                 type = { "keyword", "function", "keyword2", "normal" } },
+    { pattern = "new()%s*%w+()%f[(]",                         type = { "keyword", "function", "normal" } },
+    -- Class name reference, ( then ,
+    { pattern = "%(%s*()%w+%s*()%w+%s*%,",                    type = { "normal", "function", "normal" } },
+    { pattern = "%(%s*()%w+()%<.*%>()%s*%w+%s*%,",            type = { "normal", "function", "keyword2", "normal" } },
+    -- Class name reference, (final then ,
+    { pattern = "%(%s*()final%s*()%w+()%<.*%>()%s*%w+%s*%,",    type = { "normal", "keyword", "function", "keyword2", "normal" } },
+    { pattern = "%(%s*()final%s*()%w+%s*()%w+%s*%,",            type = { "normal", "keyword", "function", "normal" } },
+    -- Class name reference, , then ,
+    { pattern = "%s*()%w+%s+()%w+%s*%,",                      type = { "normal", "function", "normal" } },
+    { pattern = "%s*()%w+()%<.*%>()%s+%w+%s*%,",              type = { "normal", "function", "keyword2", "normal" } },
+    -- FIX: Class name reference, , then )
+    { pattern = "%s*()%w+()%s+%w+%s*%)",                      type = { "normal", "function", "normal" } },
+    { pattern = "%,%s*()%w+()%<.*%>()%s+w+%s*%)",             type = { "normal", "function", "keyword2", "normal" } },
+    -- Class name reference, ( then )
+    { pattern = "%(%s*()%w+%s*()%w+%s*%)",                    type = { "normal", "function", "normal" } },
+    { pattern = "%(%s*()%w+()%<.*%>()%s*()%w+%s*%)",          type = { "normal", "function", "keyword2", "normal" } },
+    -- ?
+    { pattern = "%s*()%w+()%<.*%>()%s+w+%s*%)",               type = { "normal", "function", "keyword2", "normal" } },
+    -- Array
+    { pattern = "%w+()%[()%d*()%]",                           type = { "function", "normal", "number", "normal" } },
+    -- Class name reference, method
+    { pattern = "%w+%s+%w+%s*()%f[(]",                        type = { "function", "normal" } },
+    { pattern = "%w+()%<.*%>()%s+%w+%s*()%f[(]",              type = { "function", "keyword2", "function", "normal" } },
+    -- other
+    -- TODO: see if therea are regex patterns that can be converted to lua patterns
+    { regex   = [[this(?=\.?\@?)]],                           type = "keyword"  }, -- this keyword
+    { pattern = "^%s*%@.+%)",                                 type = "keyword2" }, -- Annotation (at line start)
+    { regex   = [[\s*\@.+\)(?=\s+\w+)]],                      type = "keyword2" }, -- Annotation (at line middle)
+    { pattern = "%@%w+",                                      type = "keyword2" }, -- Annotation (like: final @Nullable String something;)
+    { pattern = "[A-Z][A-Z_%d]+%f[^a-zA-Z_%d]",               type = "keyword2" }, -- Constants
+    { pattern = "%:%:()%w+",                                  type = { "normal", "function" } }, -- Method reference with double colon operator
+    { pattern = "%.class",                                    type = "normal"   }, -- .class should be colored as normal
+    { pattern = "[%a_][%w_]*",                                type = "symbol"   }, -- Symbols
+    -- TODO: check for missing syntaxes by opening an example Java project
+    -- TODO: check if there are redundant/overlapping patterns
   },
   symbols = {
     ["abstract"]      = "keyword",
