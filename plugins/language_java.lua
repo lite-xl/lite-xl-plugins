7ab827a79c482f376b32765f8a92c0506a3edce2
PerilousBooklet <raffaele.orabona@protonmail.com>
2025-04-30 14:32:34 +0200

    WIP: fixing patterns....

--- plugins/language_java.lua
+++ plugins/language_java.lua
@@ -23,15 +23,25 @@ syntax.add {
     { pattern = "[%a_][%w_]*%f[(]",                           type = "function" }, -- Method
     { pattern = "^import()%s+()[%w_.]+",                      type = { "keyword", "normal", "normal" } }, -- Import
     -- Class name reference, ;
+    { pattern = "^%s*()return()%s*%.-()%;$",                  type = { "normal", "keyword", "symbol", "normal" } }, -- Fix class name reference, ; pattern
     { pattern = "^%w+%s*()%w+%s*%;",                          type = { "function", "normal" } },
     { pattern = "^%w+%s*()%<.-%>()%s*%w+%s*%;",               type = { "function", "keyword2", "normal" } },
+    -- FIX: valid only when preceded by a single keyword (es. private), when using more than one (es. private static) it breaks
+    { pattern = "^%s*%w+%s*()%w+()%<.-%>()%s*%w+%s*%;",       type = { "keyword", "function", "keyword2", "normal" } },
+    { pattern = "^%s*%w+%s*()%w+%s*()%w+%s*%;",               type = { "keyword", "function", "normal" } },
     -- Class name reference, =
+    -- FIX: paramClassDiagramTextToWrap = builder.toString();
+    -- FIX: return paramClassDiagramTextToWrap;
+    -- FIX: classDiagramText = createAsciidocWrappedDiagramText(classDiagramText);
+    -- FIX: IOUtils.write(classDiagramText, outputStream, getEncoding());
+    -- FIX: this.outputFilename = outputFilename;
+    -- FIX: if (whitelistRegexp == null || "".equals(whitelistRegexp)) {
     { pattern = "%w+%s*()%w+%s*()%=",                         type = { "function", "normal", "operator" } },
     { pattern = "%w+%s*()%<.-%>()%s*%w+%s*()%=",              type = { "function", "keyword2", "normal", "operator" } },
     -- Class name reference, new
     { pattern = "new()%s*%w+()%<.-%>()%f[(]",                 type = { "keyword", "function", "keyword2", "normal" } },
     { pattern = "new()%s*%w+()%f[(]",                         type = { "keyword", "function", "normal" } },
-    -- FIX: Class name reference, ( then ,
+    -- Class name reference, ( then ,
     { pattern = "%(%s*()%w+%s*()%w+%s*%,",                    type = { "normal", "function", "normal" } },
     { pattern = "%(%s*()%w+()%<.-%>()%s*%w+%s*%,",            type = { "normal", "function", "keyword2", "normal" } },
     -- Class name reference, (final then ,
@@ -46,12 +56,10 @@ syntax.add {
     -- Class name reference, ( then )
     { pattern = "%(%s*()%w+%s*()%w+%s*%)",                    type = { "normal", "function", "normal" } },
     { pattern = "%(%s*()%w+()%<.-%>()%s*()%w+%s*%)",          type = { "normal", "function", "keyword2", "normal" } },
-    -- ?
-    { pattern = "%s*()%w+()%<.-%>()%s+w+%s*%)",               type = { "normal", "function", "keyword2", "normal" } },
     -- Array
     { pattern = "%w+()%[()%d*()%]",                           type = { "function", "normal", "number", "normal" } },
     -- Class name reference, method
-    { pattern = "%w+%s+%w+%s*()%f[(]",                        type = { "function", "normal" } },
+    { pattern = "%w+%s+()%w+%s*()%f[(]",                      type = { "keyword", "function", "normal" } },
     { pattern = "%w+()%<.-%>()%s+%w+%s*()%f[(]",              type = { "function", "keyword2", "function", "normal" } },
     -- Other patterns
     -- TODO: see if there are regex patterns that can be converted to lua patterns
@@ -63,8 +71,10 @@ syntax.add {
     { pattern = "%:%:()%w+",                                  type = { "normal", "function" } }, -- Method reference with double colon operator
     { pattern = "%.class",                                    type = "normal"   }, -- .class should be colored as normal
     { pattern = "[%a_][%w_]*",                                type = "symbol"   }, -- Symbols
-    -- TODO: check for missing syntaxes by opening an example Java project
+    -- WIP: check for missing syntaxes by opening an example Java project
     -- TODO: check if there are redundant/overlapping patterns
+    -- TODO: private final VisibilityType maxVisibilityMethods = VisibilityType.PRIVATE;
+    --       in this case VisibilityType should be colored as function
   },
   symbols = {
     ["abstract"]      = "keyword",
