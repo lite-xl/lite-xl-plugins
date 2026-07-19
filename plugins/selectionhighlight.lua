624bed6b25fe5e8f64a80fa73afc38b36738098e
eleriaqueen <lizard.valeth@gmail.com>
2026-07-19 13:29:13 +0200

    Add config options to `selectionhighlight`: - Highlight Box Opacity - Highlight Box Thickness - Use SCALE Value

--- plugins/selectionhighlight.lua
+++ plugins/selectionhighlight.lua
@@ -1,16 +1,52 @@
 -- mod-version:3
 local style = require "core.style"
+local common = require "core.common"
+local config = require "core.config"
 local DocView = require "core.docview"
+
+config.plugins.selectionhighlight = common.merge({
+  thickness = 1,
+  use_scale = true,
+  config_spec = {
+    name = "Selection Highlight",
+    {
+      label = "Highlight Box Opacity",
+      description = "Define the how opaque the highlight box is.",
+      path = "opacity",
+      type = "number",
+      default = 255,
+      min = 0,
+      max = 255
+    },
+    {
+      label = "Highlight Box Thickness",
+      description = "Thickness of highlight box.",
+      path = "thickness",
+      type = "number",
+      default = 1,
+      min = 1,
+      max = 10
+    },
+    {
+      label = "Use SCALE Value",
+      description = "Increase thickness of highlight box acording to the SCALE value.",
+      path = "use_scale",
+      type = "toggle",
+      default = true
+    }
+  }
+}, config.plugins.selectionhighlight)
 
 -- originally written by luveti
 
-local function draw_box(x, y, w, h, color)
+local function draw_box(x, y, w, h, color, thickness, use_scale)
   local r = renderer.draw_rect
-  local s = math.ceil(SCALE)
-  r(x, y, w, s, color)
-  r(x, y + h - s, w, s, color)
-  r(x, y + s, s, h - s * 2, color)
-  r(x + w - s, y + s, s, h - s * 2, color)
+  local t = use_scale and math.ceil(SCALE) * thickness or thickness
+
+  r(x, y, w, t, color)
+  r(x, y + h - t, w, t, color)
+  r(x, y + t, t, h - t * 2, color)
+  r(x + w - t, y + t, t, h - t * 2, color)
 end
 
 
@@ -36,7 +72,10 @@ function DocView:draw_line_body(line, x, y)
           local x1 = x + self:get_col_x_offset(line, start_col)
           local x2 = x + self:get_col_x_offset(line, end_col + 1)
           local color = style.selectionhighlight or style.syntax.comment
-          draw_box(x1, y, x2 - x1, lh, color)
+          color[4] = config.plugins.selectionhighlight.opacity
+          local thickness = config.plugins.selectionhighlight.thickness
+          local use_scale = config.plugins.selectionhighlight.use_scale
+          draw_box(x1, y, x2 - x1, lh, color, thickness, use_scale)
         end
         last_col = end_col + 1
       end
