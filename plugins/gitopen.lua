d1052f71a6965f0202e75c13b6dc606ba3c94807
Bot Randomness <bot.randomness@gmail.com>
2026-08-31 12:53:59 -0400

    fix: added back in require core.config

--- plugins/gitopen.lua
+++ plugins/gitopen.lua
@@ -2,6 +2,7 @@
 local core = require "core"
 local command = require "core.command"
 local common = require "core.common"
+local config = require "core.config"
 
 local function exec(cmd)
   local proc = process.start(cmd)
